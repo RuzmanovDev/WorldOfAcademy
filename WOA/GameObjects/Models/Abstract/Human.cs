11aa30d87c95bbe991c37090590701cb6a61e24f
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-06-27 14:02:56 +0300

    Addded Trainer.cs

--- WOA/GameObjects/Models/Abstract/Human.cs
+++ WOA/GameObjects/Models/Abstract/Human.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace GameObjects.Models.Abstract
 {
-    class Human
+    using Common;
+    using GameObjects.Contracts;
+
+    public abstract class Human : IHuman
     {
+        protected int energy;
+        protected int hp;
+        protected string name;
+
+        public Human(string name, int hp, int energy)
+        {
+            this.Name = name;
+            this.HP = hp;
+            this.Energy = energy;
+        }
+
+        public int Energy
+        {
+            get
+            {
+                return this.energy;
+            }
+
+            protected set
+            {
+                Validator.CheckIfNull(value, "The energy cannot be null");
+
+                this.energy = value;
+            }
+        }
+
+        public int HP
+        {
+            get
+            {
+                return this.hp;
+            }
+
+            protected set
+            {
+                this.hp = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            protected set
+            {
+                this.name = value;
+            }
+        }
     }
 }
--- WOA/GameObjects/Models/Trainer.cs
+++ WOA/GameObjects/Models/Trainer.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GameObjects.Models
+﻿namespace GameObjects.Models
 {
-    class Trainer
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using GameObjects.Contracts;
+    using Enumerations;
+    using GameObjects.Models.Abstract;
+
+    public class Trainer : Human, ITrainer, IHuman
     {
+        private IList<IProblem> problems;
+
+        public Trainer(string name, int hp, int energy, IList<IProblem> problems, TrainerType trinerType)
+            : base(name, hp, energy)
+        {
+            this.Problems = problems;
+            this.trainerType = trinerType;
+        }
+
+        public IList<IProblem> Problems
+        {
+            get
+            {
+                return this.problems;
+            }
+
+            private set
+            {
+                this.problems = value;
+            }
+        }
+
+        public TrainerType trainerType { get; protected set; }
+
+        public void ThrowProblem(IStudent st)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
